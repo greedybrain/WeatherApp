da7f7345022af6e4ef386c257d0783c2b0ba9338
greedybrain <greedybrain18@gmail.com>
2021-01-01 15:13:23 -0500

    ...

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -6,10 +6,11 @@ const forecast = (lat, long, cb) => {
 	const { WEATHER_STACK_ACCESS_KEY: accessKey } = process.env;
 	const url = `http://api.weatherstack.com/current?access_key=${accessKey}&query=${lat},${long}&units=f`;
 	request({ url, json: true }, (err, { body } = {}) => {
-		if (err) return cb("Unable to connect to weather service");
-		if (body.error) return cb("Unable to find location");
+		if (err) return cb("Unable to connect to weather service", undefined); // undefined = no data
+		if (body.error) return cb("Unable to find location", undefined);
 		const { temperature, feelslike, humidity } = body.current;
 		cb(undefined, {
+			// undefined = no error
 			temperature,
 			feelslike,
 			humidity,

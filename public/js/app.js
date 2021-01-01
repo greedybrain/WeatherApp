7309c1cca8005262f343b5a57bd0af0b21d98d35
greedybrain <greedybrain18@gmail.com>
2021-01-01 11:42:42 -0500

    added humidity

--- public/js/app.js
+++ public/js/app.js
@@ -1,33 +1,36 @@
-const locationForm = document.querySelector('form')
-const locationInput = document.querySelector('input')
-const errMessage = document.querySelector('.p1')
-const succMessage = document.querySelector('.p2')
+const locationForm = document.querySelector("form");
+const locationInput = document.querySelector("input");
+const errMessage = document.querySelector(".p1");
+const succMessage = document.querySelector(".p2");
 
 const isLoading = () => {
-        errMessage.textContent = ''
-        succMessage.textContent = ''
-        succMessage.textContent = 'Loading...'
-}
+	errMessage.textContent = "";
+	succMessage.textContent = "";
+	succMessage.textContent = "Loading...";
+};
 
-const handleError = data => errMessage.textContent = data.error
+const handleError = (data) => (errMessage.textContent = data.error);
 
-const handleSuccess = data => succMessage.textContent = `It is ${ data.temperature } in ${ data.location }, but it feels like ${ data.feelslike }`
+const handleSuccess = (data) =>
+	(succMessage.textContent = `It is ${data.temperature} in ${data.location}, but it feels like ${data.feelslike}. The humidity is ${data.humidity}`);
 
-const getForecastInfo = async location => {
-        const url = `/weather?address=${ location }`
-        try {
-                const res = await fetch(url)
-                const data = await res.json()
-                if (data.error) return handleError(data)
-                handleSuccess(data)
-        } catch (error) { errMessage.textContent = error.message }
-}
+const getForecastInfo = async (location) => {
+	const url = `/weather?address=${location}`;
+	try {
+		const res = await fetch(url);
+		const data = await res.json();
+		if (data.error) return handleError(data);
+		handleSuccess(data);
+	} catch (error) {
+		errMessage.textContent = error.message;
+	}
+};
 
-locationForm.addEventListener('submit', e => {
-        e.preventDefault()
+locationForm.addEventListener("submit", (e) => {
+	e.preventDefault();
 
-        isLoading()
+	isLoading();
 
-        getForecastInfo(locationInput.value)  
-        e.target.reset()
-})
+	getForecastInfo(locationInput.value);
+	e.target.reset();
+});

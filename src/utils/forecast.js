c9f9e1f0b67668daebd97af264300575f1ebbb33
greedybrain <greedybrain18@gmail.com>
2021-01-01 11:51:42 -0500

    fixed humidity undefined issue

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,19 +1,20 @@
 //! NPM Modules
-const request = require('postman-request')
-const chalk = require('chalk');
+const request = require("postman-request");
+const chalk = require("chalk");
 
 const forecast = (lat, long, cb) => {
-        const { WEATHER_STACK_ACCESS_KEY: accessKey } = process.env
-        const url = `http://api.weatherstack.com/current?access_key=${ accessKey }&query=${ lat },${ long }&units=f`
-        request({ url, json: true }, (err, { body }={}) => {
-                if (err) return cb("Unable to connect to weather service")
-                if (body.error) return cb("Unable to find location")
-                const { temperature, feelslike } = body.current
-                cb(undefined, { 
-                        temperature, 
-                        feelslike 
-                })
-        }) 
-}
+	const { WEATHER_STACK_ACCESS_KEY: accessKey } = process.env;
+	const url = `http://api.weatherstack.com/current?access_key=${accessKey}&query=${lat},${long}&units=f`;
+	request({ url, json: true }, (err, { body } = {}) => {
+		if (err) return cb("Unable to connect to weather service");
+		if (body.error) return cb("Unable to find location");
+		const { temperature, feelslike, humidity } = body.current;
+		cb(undefined, {
+			temperature,
+			feelslike,
+			humidity,
+		});
+	});
+};
 
-module.exports = forecast 
+module.exports = forecast;
